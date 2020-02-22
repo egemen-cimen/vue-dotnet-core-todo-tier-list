79fc21723790f5b171d83721ab785309d039fd15
Egemen Çimen <ecimen96@gmail.com>
2020-02-22 22:20:56 +0300

    Changed router links into buttons

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,13 +1,26 @@
  <template>
   <div class="menubar">
     <div class="btn-group" role="group" aria-label="Menu bar">
-      <router-link to="/board">Kanban Tier List Board</router-link>
-      <router-link to="/backlog">Backlog</router-link>
+      <!--v-bind button classes to router links-->
+      <router-link to="/board" :class="buttonClass('/board')">Kanban Tier List Board</router-link>
+      <router-link to="/backlog" :class="buttonClass('/backlog')">Backlog</router-link>
     </div>
   </div>
 </template>
   <script>
 export default {
-  name: "MenuBar"
+  name: "MenuBar",
+  methods: {
+    /**
+     * Match buttons' router path with the current path and highlight the current path's button
+     */
+    buttonClass(path) {
+      return {
+        btn: true,
+        "btn-primary": this.$route.path === path,
+        "btn-secondary": this.$route.path !== path
+      };
+    }
+  }
 };
 </script>
