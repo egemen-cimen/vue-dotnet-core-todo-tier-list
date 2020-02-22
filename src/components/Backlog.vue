45dc43b283614f7d1dd2603ceaabe49ccf9752d0
Egemen Çimen <ecimen96@gmail.com>
2020-02-22 21:31:41 +0300

    Added cards to backlog

--- src/components/Backlog.vue
+++ src/components/Backlog.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="backlog-view">
     <new-item-list></new-item-list>
-    <p>Backlog</p>
+    <div class="card" v-for="item in items" :key="item.id">
+          <div class="card-block">
+            <h5 class="card-title"><span class="text-muted">#{{item.id}}</span>
+            {{item.text}}</h5>
+          </div>
+      </div>
+    <p>Total items in the Backlog:</p>
   </div>
 </template>
 
 <script>
+/**
+ * Connect the store to retrieve data (using mapState method)
+ */ 
+import { mapState } from "vuex";
 import NewItemListForm from "./NewItemListForm";
 
 export default {
   name: "Backlog",
   components: {
-    "new-item-list": NewItemListForm
-  }
+    "new-item-list": NewItemListForm,
+  },
+  computed: mapState({
+items: s => s.items.todoItems,
+
+
+  })
 };
 </script>
