9a65dac8a704639901a3a5572ec068464ce64277
Egemen Çimen <ecimen96@gmail.com>
2020-02-22 21:08:17 +0300

    Added a mutation to record todo list items

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,24 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+/**
+ * The mutation named addListItem is a method which takes the current state
+ * and an item as arguments.
+ * 
+ * The item's then pushed into state.items.todoItems using Object.assign.
+ * 
+ */
 export default new Vuex.Store({
     state: {
         items: {
-            todo: [],
+            todoItems: [],
         },
-        nextId: 1,
+        itemId: 0,
+    },
+    mutations: {
+        addListItem(state, item) {
+            state.itemId += 1;
+            state.items.todoItems.push(Object.assign(item, { id: state.itemId}));
+        }
     },
 });
