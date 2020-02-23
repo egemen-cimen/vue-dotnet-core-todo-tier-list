241c8e097afb25f67e68f295e6df9fa8cc1f57f7
Egemen Çimen <ecimen96@gmail.com>
2020-02-23 13:26:25 +0300

    Added task column and task column items to kanban tier list board

---
+++ src/components/TaskColumn.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <h3 class="card-header">{{title}}</h3>
+    <div class="card-body">
+      <div v-for="item in items" :key="item.id">
+        <item :item="item"></item>
+      </div>
+    </div>
+    <div class="card-footer text-muted">{{itemCount}}</div>
+  </div>
+</template>
+
+<script>
+import TaskColumnItem from "@/components/TaskColumnItem";
+export default {
+  name: "TaskColumn",
+  props: ["items", "title", "id"],
+  components: {
+    item: TaskColumnItem
+  },
+  computed: {
+    itemCount() {
+      if (!this.items) return "";
+      if (this.items.length === 1) return "1 item";
+      return `${this.items.length} items`;
+    }
+  }
+};
+</script>>
---
+++ src/components/TaskColumnItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card task-column-item">
+    <div class="card-block">
+      <h5 class="card-title">
+        <span class="text-muted">#{{item.id}}</span>
+        {{item.text}}
+      </h5>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "TaskColumnItem",
+  props: ["item"]
+};
+</script>
+
+<style>
+@import "../assets/styles/taskColumnItem.css";
+</style>
