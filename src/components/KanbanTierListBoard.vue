89085c97446b2fecfecdd4a0c02bf9be63c59f26
Egemen Çimen <ecimen96@gmail.com>
2020-02-29 18:41:53 +0300

    Added colors to tiers

--- src/components/KanbanTierListBoard.vue
+++ src/components/KanbanTierListBoard.vue
@@ -2,17 +2,17 @@
   <div class="board">
     <div class="row">
       <div class="col-md">
-        <task-column id="backlogItems" title="Backlog" :items="backlogListItems"></task-column>
+        <task-column id="backlogItems" title="Backlog" class="card-backlog" :items="backlogListItems"></task-column>
       </div>
       <div class="col-md">
-        <task-column id="sTodoItems" title="S Tier" :items="sTodoListItems"></task-column>
-        <task-column id="aTodoItems" title="A Tier" :items="aTodoListItems"></task-column>
-        <task-column id="bTodoItems" title="B Tier" :items="bTodoListItems"></task-column>
-        <task-column id="cTodoItems" title="C Tier" :items="cTodoListItems"></task-column>
-        <task-column id="dTodoItems" title="D Tier" :items="dTodoListItems"></task-column>
+        <task-column id="sTodoItems" title="S Tier" class="card-sTier" :items="sTodoListItems"></task-column>
+        <task-column id="aTodoItems" title="A Tier" class="card-aTier" :items="aTodoListItems"></task-column>
+        <task-column id="bTodoItems" title="B Tier" class="card-bTier" :items="bTodoListItems"></task-column>
+        <task-column id="cTodoItems" title="C Tier" class="card-cTier" :items="cTodoListItems"></task-column>
+        <task-column id="dTodoItems" title="D Tier" class="card-dTier" :items="dTodoListItems"></task-column>
       </div>
       <div class="col-md">
-        <task-column id="doneItems" title="Done" :items="doneListItems"></task-column>
+        <task-column id="doneItems" title="Done" class="card-done" :items="doneListItems"></task-column>
       </div>
     </div>
   </div>
