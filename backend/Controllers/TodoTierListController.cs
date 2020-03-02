23d42e9e49cba6b2b312bed4c52aff9c8907eaea
Egemen Çimen <ecimen96@gmail.com>
2020-03-02 23:43:31 +0300

    Implemented an in-memory store for the API

--- backend/Controllers/TodoTierListController.cs
+++ backend/Controllers/TodoTierListController.cs
@@ -13,36 +13,26 @@ namespace backend.Controllers
     {
         private readonly ILogger<TodoTierListController> _logger;
 
+        private List<TodoListItem> todoItems;
+
         public TodoTierListController(ILogger<TodoTierListController> logger)
         {
             _logger = logger;
+            todoItems = Program.todoItems;
         }
 
         [HttpGet]
         public IEnumerable<TodoListItem> Get()
         {
-            var i = 0;
-            var values = Enum.GetValues(typeof(TodoColumn));
-            var rng = new Random();
-
-            return Enumerable.Range(1, 5).Select(index => new TodoListItem
-            {
-                Id = i++,
-                Text = "Text_" + i,
-                Status = (TodoColumn)values.GetValue(rng.Next(values.Length))
-            })
-            .ToArray();
+            return todoItems.ToArray();
         }
 
         [HttpPut]
         public int Put(TodoListItem todoListItem)
         {
-            if (todoListItem.Id == -1)
-            {
-                Console.WriteLine(todoListItem.Text + "___" + todoListItem.Id + "___" + todoListItem.Status);
-                return 6;
-            }
-            return -1;
+            todoListItem.Id = todoItems.Count;
+            todoItems.Add(todoListItem);
+            return todoListItem.Id;
         }
     }
 }
--- backend/Program.cs
+++ backend/Program.cs
@@ -11,8 +11,10 @@ namespace backend
 {
     public class Program
     {
+        public static List<TodoListItem> todoItems;
         public static void Main(string[] args)
         {
+            todoItems = new List<TodoListItem>();
             CreateHostBuilder(args).Build().Run();
         }
 
